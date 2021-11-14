950784124b52926ac1cedf113b374b0c0d290a05
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2021-11-14 08:34:51 +0100

    completed intro section

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -7,7 +7,13 @@ module.exports = {
             fontFamily: {
                 'body': ['Open Sans'],
                 'heading': ['Raleway']
-            }
+            },
+            backgroundColor: theme => ({
+                ...theme('colors'),
+                'button-bg': 'hsl(224, 93%, 58%)',
+                'button-hover-bg': 'hsl(170, 45%, 43%)'
+
+            })
         },
     },
     variants: {
