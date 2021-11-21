f4aaa309c58fb873e9701e4648ecd082e584b509
Shreeraam Alagarsamy Sethuraj <shreeraaam@outlook.com>
2021-11-21 08:14:30 +0100

    added waves svg file

--- tailwindcss/tailwind.config.js
+++ tailwindcss/tailwind.config.js
@@ -8,6 +8,16 @@ module.exports = {
                 'body': ['Open Sans'],
                 'heading': ['Raleway']
             },
+            borderRadius: {
+                'none': '0',
+                'sm': '0.125rem',
+                DEFAULT: '0.25rem',
+                DEFAULT: '4px',
+                'md': '0.375rem',
+                'lg': '0.5rem',
+                'full': '9999px',
+                'custom': '100px',
+            },
             backgroundColor: theme => ({
                 ...theme('colors'),
                 'button-bg': 'hsl(224, 93%, 58%)',
